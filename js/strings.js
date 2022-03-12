f34768c4f858f86bcd4f156b23eb0b45a20250a6
quideptrai <99940428+quideptrai@users.noreply.github.com>
2022-03-12 21:48:37 +0700

    + init

---
+++ js/strings.js
@@ -0,0 +1,29 @@
+var download_link_ios_http = "http://tai.sieuno.vin/ios/";
+var download_link_android_http = "http://tai.sieuno.vin/android/";
+var download_link_ios = "https://tai.sieuno.vin/ios/";
+var download_link_android = "https://tai.sieuno.vin/android/";
+
+var download_link_http_1 = "http://dl.sieuno.vin/";
+var download_link_http_2 = "http://tai.sieuno.vin/";
+var download_link_http_3 = "http://download.sieuno.vin/";
+var download_link_https_1 = "https://dl.sieuno.vin/";
+var download_link_https_2 = "https://tai.sieuno.vin/";
+var download_link_https_3 = "https://download.sieuno.vin/";
+
+var download_otp_android_url = "https://tai.sieuno.vin/NoHuOTP.apk";
+var download_otp_ios_url = "itms-services://?action=download-manifest&url=https://tai.sieuno.vin/SieuNoOTP.plist";
+
+var download_ipa_ios_url = "itms-services://?action=download-manifest&url=https://tai.sieuno.vin/SieuNo.plist";
+var download_app_store_ios_url = "";
+
+var download_apk_android_url = "https://tai.sieuno.vin/NoHu.apk";
+var download_ch_play_android_url = "https://linkngan.net/snandroid";
+
+var facebook_fanpage_url = "https://www.facebook.com/Si%C3%AAu-H%C5%A9-Ti%E1%BB%81n-v%E1%BB%81-nh%C6%B0-l%C5%A9-106873147745604/"; // https://www.facebook.com/Si%C3%AAu-H%C5%A9-Ti%E1%BB%81n-v%E1%BB%81-nh%C6%B0-l%C5%A9-106873147745604/
+var facebook_group_url = "https://www.facebook.com/groups/2581596762084844/";
+
+var vpn_chrome_url = "https://chrome.google.com/webstore/detail/touch-vpn-secure-and-unli/bihmplhobchoageeokmgbdihknkjbknd?hl=en";
+var vpn_firefox_url = "https://addons.mozilla.org/en-US/firefox/addon/touch-vpn/";
+var vpn_ios_url = "https://apps.apple.com/us/app/touch-vpn-unlimited-proxy/id991744383"
+var vpn_android_url = "https://play.google.com/store/apps/details?id=com.cloudflare.onedotonedotonedotone&hl=en"
+var nohu_url = "https://sieuno.vin/";
